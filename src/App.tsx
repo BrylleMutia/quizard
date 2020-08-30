7ebee3f4000c00e5f3368cc4b3125534bb83f9c4
Brylle Mutia <bryllemutia3@gmail.com>
2020-08-30 18:05:45 +0800

    updated loading sequence

--- src/App.tsx
+++ src/App.tsx
@@ -29,100 +29,100 @@ const App = (props: any) => {
         setGameOver(false);
         setIsFirstTry(false);
 
-        // const newQuestions = await fetchQuestions(TOTAL_QUESTIONS, DIFFICULTY);
+        const newQuestions = await fetchQuestions(TOTAL_QUESTIONS, DIFFICULTY);
 
-        const newQuestions = [
-            {
-                category: "Animals",
-                type: "multiple",
-                difficulty: "easy",
-                question: "The K\u0101k\u0101p\u014d is a large, flightless, nocturnal parrot native to which country?",
-                correct_answer: "New Zealand",
-                incorrect_answers: ["South Africa", "Australia", "Madagascar"],
-                answers: ["South Africa", "Australia", "Madagascar", "New Zealand"],
-            },
-            {
-                category: "Science & Nature",
-                type: "multiple",
-                difficulty: "easy",
-                question: "Which of the following blood vessels carries deoxygenated blood?",
-                correct_answer: "Pulmonary Artery",
-                incorrect_answers: ["Pulmonary Vein", "Aorta", "Coronary Artery"],
-                answers: ["Pulmonary Vein", "Aorta", "Coronary Artery", "Pulmonary Artery"],
-            },
-            {
-                category: "Entertainment: Cartoon & Animations",
-                type: "multiple",
-                difficulty: "easy",
-                question: "In the Pixar film, &quot;Toy Story&quot; what was the name of the child who owned the toys?",
-                correct_answer: "Andy",
-                incorrect_answers: ["Edward", "Danny", "Matt"],
-                answers: ["Edward", "Danny", "Matt", "Andy"],
-            },
-            {
-                category: "Entertainment: Video Games",
-                type: "multiple",
-                difficulty: "easy",
-                question: "In the first game of the Sly Cooper franchise, what family heirloom did Sly Cooper want to steal back?",
-                correct_answer: "Thievius Raccoonus",
-                incorrect_answers: ["Raccoon Training 101", "The Art of Sneak", "Raccoonus Teachus"],
-                answers: ["Raccoon Training 101", "The Art of Sneak", "Raccoonus Teachus", "Thievius Raccoonus"],
-            },
-            {
-                category: "Entertainment: Video Games",
-                type: "multiple",
-                difficulty: "easy",
-                question: "What is Gabe Newell&#039;s favorite class in Team Fortress 2?",
-                correct_answer: "Spy",
-                incorrect_answers: ["Heavy", "Medic", "Pyro"],
-                answers: ["Heavy", "Medic", "Pyro", "Spy"],
-            },
-            {
-                category: "Entertainment: Film",
-                type: "multiple",
-                difficulty: "easy",
-                question: "In &quot;Jurassic World&quot;, what is the name of the dinosaur that is a genetic hybrid?",
-                correct_answer: "Indominus Rex",
-                incorrect_answers: ["Mosasaurus", "Pteranodon", "Tyrannosaurus Rex "],
-                answers: ["Mosasaurus", "Pteranodon", "Tyrannosaurus Rex ", "Indominus Rex"],
-            },
-            {
-                category: "Entertainment: Video Games",
-                type: "multiple",
-                difficulty: "easy",
-                question: "What is the homeworld of the Elites from Halo?",
-                correct_answer: "Sanghelios",
-                incorrect_answers: ["Te", "Doisac", "Eayn"],
-                answers: ["Te", "Doisac", "Eayn", "Sanghelios"],
-            },
-            {
-                category: "Vehicles",
-                type: "multiple",
-                difficulty: "easy",
-                question: "The LS1 engine is how many cubic inches?",
-                correct_answer: "346",
-                incorrect_answers: ["350", "355", "360"],
-                answers: ["350", "355", "360", "346"],
-            },
-            {
-                category: "Mythology",
-                type: "multiple",
-                difficulty: "easy",
-                question: "The Nike apparel and footwear brand takes it&#039;s name from the Greek goddess of what?",
-                correct_answer: "Victory",
-                incorrect_answers: ["Courage", "Strength", "Honor"],
-                answers: ["Courage", "Strength", "Honor", "Victory"],
-            },
-            {
-                category: "Entertainment: Television",
-                type: "multiple",
-                difficulty: "easy",
-                question: "In the cartoon &#039;SpongeBob SquarePants&#039;, what did the acronym E.V.I.L stand for?",
-                correct_answer: "Every Villain Is Lemons",
-                incorrect_answers: ["Every Villain Is Lemonade", "Every Villain Is Limes", "Each Villain Is Lemonade"],
-                answers: ["Every Villain Is Lemonade", "Every Villain Is Limes", "Each Villain Is Lemonade", "Every Villain Is Lemons"],
-            },
-        ];
+        // const newQuestions = [
+        //     {
+        //         category: "Animals",
+        //         type: "multiple",
+        //         difficulty: "easy",
+        //         question: "The K\u0101k\u0101p\u014d is a large, flightless, nocturnal parrot native to which country?",
+        //         correct_answer: "New Zealand",
+        //         incorrect_answers: ["South Africa", "Australia", "Madagascar"],
+        //         answers: ["South Africa", "Australia", "Madagascar", "New Zealand"],
+        //     },
+        //     {
+        //         category: "Science & Nature",
+        //         type: "multiple",
+        //         difficulty: "easy",
+        //         question: "Which of the following blood vessels carries deoxygenated blood?",
+        //         correct_answer: "Pulmonary Artery",
+        //         incorrect_answers: ["Pulmonary Vein", "Aorta", "Coronary Artery"],
+        //         answers: ["Pulmonary Vein", "Aorta", "Coronary Artery", "Pulmonary Artery"],
+        //     },
+        //     {
+        //         category: "Entertainment: Cartoon & Animations",
+        //         type: "multiple",
+        //         difficulty: "easy",
+        //         question: "In the Pixar film, &quot;Toy Story&quot; what was the name of the child who owned the toys?",
+        //         correct_answer: "Andy",
+        //         incorrect_answers: ["Edward", "Danny", "Matt"],
+        //         answers: ["Edward", "Danny", "Matt", "Andy"],
+        //     },
+        //     {
+        //         category: "Entertainment: Video Games",
+        //         type: "multiple",
+        //         difficulty: "easy",
+        //         question: "In the first game of the Sly Cooper franchise, what family heirloom did Sly Cooper want to steal back?",
+        //         correct_answer: "Thievius Raccoonus",
+        //         incorrect_answers: ["Raccoon Training 101", "The Art of Sneak", "Raccoonus Teachus"],
+        //         answers: ["Raccoon Training 101", "The Art of Sneak", "Raccoonus Teachus", "Thievius Raccoonus"],
+        //     },
+        //     {
+        //         category: "Entertainment: Video Games",
+        //         type: "multiple",
+        //         difficulty: "easy",
+        //         question: "What is Gabe Newell&#039;s favorite class in Team Fortress 2?",
+        //         correct_answer: "Spy",
+        //         incorrect_answers: ["Heavy", "Medic", "Pyro"],
+        //         answers: ["Heavy", "Medic", "Pyro", "Spy"],
+        //     },
+        //     {
+        //         category: "Entertainment: Film",
+        //         type: "multiple",
+        //         difficulty: "easy",
+        //         question: "In &quot;Jurassic World&quot;, what is the name of the dinosaur that is a genetic hybrid?",
+        //         correct_answer: "Indominus Rex",
+        //         incorrect_answers: ["Mosasaurus", "Pteranodon", "Tyrannosaurus Rex "],
+        //         answers: ["Mosasaurus", "Pteranodon", "Tyrannosaurus Rex ", "Indominus Rex"],
+        //     },
+        //     {
+        //         category: "Entertainment: Video Games",
+        //         type: "multiple",
+        //         difficulty: "easy",
+        //         question: "What is the homeworld of the Elites from Halo?",
+        //         correct_answer: "Sanghelios",
+        //         incorrect_answers: ["Te", "Doisac", "Eayn"],
+        //         answers: ["Te", "Doisac", "Eayn", "Sanghelios"],
+        //     },
+        //     {
+        //         category: "Vehicles",
+        //         type: "multiple",
+        //         difficulty: "easy",
+        //         question: "The LS1 engine is how many cubic inches?",
+        //         correct_answer: "346",
+        //         incorrect_answers: ["350", "355", "360"],
+        //         answers: ["350", "355", "360", "346"],
+        //     },
+        //     {
+        //         category: "Mythology",
+        //         type: "multiple",
+        //         difficulty: "easy",
+        //         question: "The Nike apparel and footwear brand takes it&#039;s name from the Greek goddess of what?",
+        //         correct_answer: "Victory",
+        //         incorrect_answers: ["Courage", "Strength", "Honor"],
+        //         answers: ["Courage", "Strength", "Honor", "Victory"],
+        //     },
+        //     {
+        //         category: "Entertainment: Television",
+        //         type: "multiple",
+        //         difficulty: "easy",
+        //         question: "In the cartoon &#039;SpongeBob SquarePants&#039;, what did the acronym E.V.I.L stand for?",
+        //         correct_answer: "Every Villain Is Lemons",
+        //         incorrect_answers: ["Every Villain Is Lemonade", "Every Villain Is Limes", "Each Villain Is Lemonade"],
+        //         answers: ["Every Villain Is Lemonade", "Every Villain Is Limes", "Each Villain Is Lemonade", "Every Villain Is Lemons"],
+        //     },
+        // ];
 
         setQuestions(newQuestions);
         setScore(0);
@@ -197,13 +197,20 @@ const App = (props: any) => {
                             </React.Fragment>
                         ) : null}
 
-                        {!gameOver ? (
+                        {!gameOver && !loading ? (
                             <Typography variant="h6" style={{ marginTop: "3em" }} className={classes.score}>
                                 SCORE: {score}
                             </Typography>
                         ) : null}
 
-                        {loading && <CircularProgress />}
+                        {loading && (
+                            <React.Fragment>
+                                <Typography variant="h6" color="secondary" style={{ margin: "0.7em 0"}}>
+                                    Preparing questions...
+                                </Typography>
+                                <CircularProgress color="secondary" />
+                            </React.Fragment>
+                        )}
 
                         {!loading && !gameOver && (
                             <QuestionCard
